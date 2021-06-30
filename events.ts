79721537a823008dded5272c5e635e2786644fc0
kallefrombosnia <kallegowild@gmail.com>
2021-07-01 00:15:07 +0200

    events list update

--- events.ts
+++ events.ts
@@ -1,4 +1,28 @@
 /*
+  Player equipment buyed on server
+*/
+
+interface BuyEquip{
+  event_name: 'buy_equipment';
+  weapon_id: string | number;
+  weapon_buyer: string;
+  weapon_price: number;
+  weapon_name: string;
+  weapon_altname: string;
+}
+
+// User kalle buys deagle for 650$
+const equipresponse : BuyEquip = {
+  event_name: 'buy_equipment',
+  weapon_id: 26,
+  weapon_buyer: 'STEAM_0:1:115179770',
+  weapon_price: 650,
+  weapon_name: "deagle",
+  weapon_altname: "nighthawk"
+}
+
+
+/*
   Say event
 */
 
@@ -30,6 +54,7 @@ interface KillEvent{
   victim_id: string;
   victim_flashed: boolean;
   suicide: boolean;
+  wallbang: boolean;
   suicide_reason: 'weapon_hegrenade' | 'fall' | 'weapon_c4' | '';
 }
 
@@ -43,8 +68,37 @@ const killresponse : KillEvent = {
   victim_id: 'STEAM_0:1:115179770',
   victim_flashed: false,
   suicide: false,
+  wallbang: false,
   suicide_reason: ''
 }
+
+
+
+
+/**
+ * Damage
+ * Event when player gets damage
+ */
+
+interface DamageEvent{
+  event_name: 'damage',
+  weapon_id: number;
+  attacker_id: string;
+  health_reduced: number;
+  health: number;
+  victim_id: string;
+}
+
+// User kalle kills damper with deagle - headshot
+const damageresponse : DamageEvent = {
+  event_name: 'damage',
+  weapon_id: 26,
+  attacker_id: 'STEAM_0:1:115179770',
+  victim_id: 'STEAM_0:1:115179770',
+  health_reduced: 20,
+  health: 80
+}
+
 
 
 
@@ -171,6 +225,7 @@ const c4dropresponse : C4_Drop = {
 
 /*
   C4 pickup
+  Event sent when user pickups C4 
 */
 
 interface C4_Pickup{
@@ -195,10 +250,120 @@ interface CasterObserveEvent{
 }
 
 // User kalle picked up C4
-const casterobserveresponse : CasterObserveEvent = {
+const casterobserveevent : CasterObserveEvent = {
   event_name: 'caster_observed_player',
   user_pick_id: 'STEAM_0:1:115179770',
 }
+
+
+
+/**
+  Change weapon
+  Called when player switches current weapon
+*/
+
+interface SwitchCurrentWeapon{
+  event_name: 'weapon_switched';
+  user_id: string; 
+  weapon_id: number;
+}
+
+// User kalle changed weapon to deagle
+const weaponswitch : SwitchCurrentWeapon = {
+  event_name: 'weapon_switched',
+  user_id: 'STEAM_0:1:115179770',
+  weapon_id: 27
+}
+
+
+/**
+  Pickup item
+  Called when player pickups something including defuse kit
+*/
+
+interface PickupEvent{
+  event_name: 'pickup_item';
+  user_id: string; 
+  item_id: number;
+}
+
+// User kalle changed pickups weapon deagle
+const pickup : PickupEvent = {
+  event_name: 'pickup_item',
+  user_id: 'STEAM_0:1:115179770',
+  item_id: 27
+}
+
+
+/**
+  Drop item
+  Called when player drops something 
+*/
+
+interface DropEvent{
+  event_name: 'drop_item';
+  user_id: string; 
+  item_id: number;
+}
+
+// User kalle have dropped weapon deagle
+const drop : DropEvent = {
+  event_name: 'drop_item',
+  user_id: 'STEAM_0:1:115179770',
+  item_id: 27
+}
+
+
+
+/**
+  Money event
+  Called when players money changes
+*/
+
+interface MoneyEvent{
+  event_name: 'money_change';
+  user_id: string; 
+  current_money: number;
+}
+
+// User kalle have now has 3700$
+const money : MoneyEvent = {
+  event_name: 'money_change',
+  user_id: 'STEAM_0:1:115179770',
+  current_money: 3700
+}
+
+
+/**
+  Round win event
+  Called when players money changes
+*/
+
+interface RoundEndEvent{
+  event_name: 'round_end';
+  side_win: 'tt' | 'ct'; 
+  tt_rounds: number;
+  ct_rounds: number;
+}
+
+// User kalle have now has 3700$
+const moeny : MoneyEvent = {
+  event_name: 'money_change',
+  user_id: 'STEAM_0:1:115179770',
+  current_money: 3700
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
